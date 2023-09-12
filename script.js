49823b0007bbd510a9e6261d0059fba7fdab8487
Asturuf2 <02biglernoah@gmail.com>
2023-09-12 12:02:37 -0500

    added multicolor and button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const container = document.querySelector("#container")
 
+const div = document.querySelector(".buttonContainer");
+div.style.display = "flex";
+div.style.justifyContent = "center";
+
+div.addEventListener("click", () => {
+    location.reload()
+})
+
 let number = Number(prompt("Input a number"))
 
 const createGrid = (number) => {
@@ -21,11 +29,14 @@ const createGrid = (number) => {
 
 createGrid(number)
 
+
 const whichColumn = document.querySelectorAll(".column")
 
 for (let i = 0; i < whichColumn.length; i++){
     whichColumn[i].addEventListener("mouseover", () => {
-        whichColumn[i].style.backgroundColor = "grey";
-        console.log(whichColumn[i]);
+        const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+        whichColumn[i].style.backgroundColor = randomColor;
     })
 }
+
+
